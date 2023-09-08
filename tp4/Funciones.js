030bdc20be518f59901699ec8ba0c5a62e4a56d0
julyjfg <80898439+julyjfg@users.noreply.github.com>
2023-09-08 20:09:17 -0300

    Add files via upload

--- tp4/Funciones.js
+++ tp4/Funciones.js
@@ -6,10 +6,11 @@ function programa() {
 
       break
     case 2:
-juego()
-    break
+    fondoEnMovimiento()
+    juego()
+      break
     case 3:
-    preload()
+
       text("Pantalla Gano", windowWidth/2, windowHeight/2)
       break
     case 4:
@@ -20,104 +21,65 @@ juego()
       break
   }
 }
-function juego(){
-personaje()
+function juego() {
+  personaje()
 }
 
 function personaje() {
   if (okIzquierda) {
-    posXDelJugador-=1.5
+    posXDelJugador-=2.5
   }
   if (okDerecha) {
-    posXDelJugador+=1.5
+    posXDelJugador+=2.5
   }
   if (okArriba) {
-    posYDelJugador-=1.5
+    posYDelJugador-=2.5
   }
   if (okAbajo) {
-    posYDelJugador+=1.5
+    posYDelJugador+=2.5
   }
   push()
-  fill(120)
+    fill(120)
     rect(posXDelJugador, posYDelJugador, tamanio, tamanio/2)
-    disparo()
-pop()
-    //disparo()
+    disparo(posXDelJugador, posYDelJugador)
+    pop()
 }
 
-function disparo() {
-
-  if (disparar == true && recarga == true) {
-
-    //SI PRESIONO Y ESTA RECARGADA EL ARMA,
-    //AGREGO VALORES AL ARREGLO CON 'push'
-    // arreglo.push(valor)
-    balasX.push(150) //AQUI AGREGAMOS OTRA CASILLA AL ARREGLO
-      // 'balasY' , con el valor que querramos para el proximo proyectil
-
-      cuenta = 0 // reiniciamos cuenta de recarga
-      recarga = false //le 'recarga' sea 'false', para que no se pueda volver a disparar
+function disparo(posX, posY) {
+  posXDelDisparo=posX+50
+    posYDelDisparo=posY
+    if (disparar==false) {
+    ellipse(posXDelDisparo, posYDelDisparo, 20, 20)
   }
-
-  // HACEMOS FOR PARA ANIMAR TODAS LAS balas
+  if (disparar == true && recarga == true) {
+    balasX.push(posXDelDisparo)
+      cuenta = 0
+      recarga = false
+  }
   for (let i = 0; i < balasX.length; i++) {
-    // 10, podria ser velocidad de la bala, en una variable
-    balasX[i]+=   10
-
-      //Si la bala ya cumple su trayectoria, con splice() borramos ese casillero
-
+    balasX[i]+= 20
       if (balasX[i] <= 0) {
- 
       balasX.splice(i, 1)
     }
-    
-    circle(balasX[i],height/2, 20)
-  } 
+    //circle(balasX[i], posY, 20)
+    ellipse(balasX[i], posYDelDisparo, 20, 20)
+  }
   cuenta++
     if (cuenta >= 30) {
     recarga = true
   }
   print("Cantidad de balas:", balasX.length)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function disparo() {
- 
-  if (okDisparo) {
-if(posXDelDisparo<window.innerWidth){
-  posXDelDisparo+=1.5
-}
  }
- ellipse(posXDelDisparo,posYDelDisparo,20,20)
- }
+function fondoEnMovimiento(){
+ push()
+ scale(2)
+  posXDelFondo-=5
+  if(posXDelFondo<=-width){
+  posXDelFondo=width
+  } 
+  image(fondo,posXDelFondo,0)
+pop()
+}
 /*function tiempo() {
  if (!okTiempo) {
  segundos = intervalo-int(millis()/1000)
@@ -138,15 +100,18 @@ if(posXDelDisparo<window.innerWidth){
  }
  }
  }*/
-
+/*function enemigos(){
+switch 
+}
+}*/
 function botones() {
   switch (pantallas) {
   case 1:
-      generarBoton(780, 610, ancho, alto, colorBoton, "Play", 780, 610, colorTexto)
+    generarBoton(780, 610, ancho, alto, colorBoton, "Play", 780, 610, colorTexto)
       generarBoton(780, 710, ancho, alto, colorBoton, "Creditos", 780, 710, colorTexto)
       break
-  case 3:
-  break
+    case 3:
+    break
   }
 }
 
--- tp4/Tp4.js
+++ tp4/Tp4.js
@@ -1,39 +1,56 @@
 let pantallas
-let okTiempo, okVidas, okArriba, okAbajo, okIzquierda, okDerecha,okDisparo, okGano
-let posXDelJugador, posYDelJugador,posXDelDisparo, posYDelDisparo, posXDatos, posYDatos,posXBoton
+
+let fondo =[]
+let botonInicio
+let botonCreditos
+
+let okTiempo 
+let okVidas
+let okArriba
+let okAbajo
+let okIzquierda
+let okDerecha
+let okGano
+let disparar 
+let recarga = true
+let enemigosActivos
+
+let posXDelJugador
+let posYDelJugador
+let posXDelDisparo
+let posYDelDisparo
+
+let velocidadFondo
+let posXDelFondo
+
+let posXDatos
+let posYDatos
+
 let w, h
 let tamanio,ancho, alto
+
 let vidas = []
-let balasX= []
+let enemigos= []
+
 let tiempoEnPantalla
 let segundos 
 let minutos
 let intervalo
+
 let colorBoton,colorTexto
 
-function setup() {
-    
-   preload()
-    
-    textSize(100)
-    textAlign(CENTER)
-    rectMode(CENTER)
-    ellipseMode(CENTER)
-    
-    for(let i=0; i<balasX.length;i++){
-      balasX[i]= windowWidth
-    }
-    
-    createCanvas(w, h)
+ 
+let cuenta 
+let balasX = []
+
+function preload(){//carga de imagenes
+fondo= loadImage("./data/fondo.jpg")
 }
 
-function draw() {
-  programa()
-}
 
-function preload() {
-    pantallas=1
-    
+function setup() {   
+  pantallas=1
+  
     w=window.innerWidth
     h=window.innerHeight
 
@@ -43,22 +60,70 @@ function preload() {
 
     posXDelJugador=100
     posYDelJugador=500
-    posXDelDisparo=posXDelJugador+50
-    posYDelDisparo=500
     
-
-    colorBoton=255
-    colorTexto=0
+    velocidadFondo=3
+    posXDelFondo=0
+ 
+ 
 
     okVidas=false
     okTiempo=false
     okGano=false
+    disparar=false
+    recargar=true
+    
+    
 
     minutos=2
     intervalo=60
     segundos=0
     tiempoEnPantalla="3:00"
+    
+    textSize(100)
+    textAlign(CENTER)
+    rectMode(CENTER)
+   imageMode(CENTER)
+    
+    for(let i=0; i<balasX.length;i++){
+      balasX[i]= windowWidth
+    }
+    for (let i=1;i<=3;i++){
+      enemigos[i]=i
+    }
+    
+    createCanvas(w, h)
+    fondo.resize(w,h)
 }
+
+function draw() {
+  image(fondo,0,0)
+  programa()
+}
+
+//function preload() {
+//      posXDelJugador=100
+//    posYDelJugador=500
+ 
+    
+
+//    colorBoton=255
+//    colorTexto=0
+
+//    okVidas=false
+//    okTiempo=false
+//    okGano=false
+//    disparar=false
+//    recargar=true
+    
+    
+
+//    minutos=2
+//    intervalo=60
+//    segundos=0
+//    tiempoEnPantalla="3:00"
+    
+   
+//}
 function mousePressed() {
   pantallas++
     print(mouseX, mouseY)
@@ -81,11 +146,12 @@ function keyPressed() {
   }
   if(key==" "){
     print("FUEGO!!!")
-  okDisparo=true
+  disparar=true
   }
   if((key=="r")||(key=="R")){
-  pantallas=2
   preload()
+    pantallas=2
+
   }
 }
 function keyReleased() {
@@ -103,6 +169,6 @@ function keyReleased() {
   }
   if(key==" "){
     print("ALTO EL FUEGO!!")
-  okDisparo=false
+  disparar=false
   }
 }
