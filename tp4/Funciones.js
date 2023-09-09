598cf4cbed3fee327d5be00ffea98b17dc407b5d
julyjfg <80898439+julyjfg@users.noreply.github.com>
2023-09-09 18:32:44 -0300

    Add files via upload

--- tp4/Funciones.js
+++ tp4/Funciones.js
@@ -2,57 +2,58 @@ function programa() {
   background(127)
     switch(pantallas) {
   case 1:
-    text("Pantalla Inicio", windowWidth/2, windowHeight/2)
-
+    inicio()
       break
     case 2:
-    fondoEnMovimiento()
-    juego()
+    instrucciones()
       break
     case 3:
-
-      text("Pantalla Gano", windowWidth/2, windowHeight/2)
+    juego()
       break
     case 4:
-    text("Pantalla Perdio", windowWidth/2, windowHeight/2)
+    pantallaGano()
       break
     case 5:
-    text("Pantalla Creditos", windowWidth/2, windowHeight/2)
+    pantallaPerdio()
+      break
+    case 6:
+    creditos()
       break
   }
+  
 }
 function juego() {
-  personaje()
+  fondoEnMovimiento()
+    interfaz()
+    enemigos()
+    personaje()
 }
 
 function personaje() {
   if (okIzquierda) {
-    posXDelJugador-=2.5
+    posXDelJugador-=3.5
   }
   if (okDerecha) {
-    posXDelJugador+=2.5
+    posXDelJugador+=3.5
   }
   if (okArriba) {
-    posYDelJugador-=2.5
+    posYDelJugador-=3.5
   }
   if (okAbajo) {
-    posYDelJugador+=2.5
+    posYDelJugador+=3.5
   }
   push()
-    fill(120)
-    rect(posXDelJugador, posYDelJugador, tamanio, tamanio/2)
+    scale(1)
+    image(nave, posXDelJugador, posYDelJugador)
     disparo(posXDelJugador, posYDelJugador)
     pop()
 }
 
 function disparo(posX, posY) {
-  posXDelDisparo=posX+50
-    posYDelDisparo=posY
-    if (disparar==false) {
-    ellipse(posXDelDisparo, posYDelDisparo, 20, 20)
-  }
-  if (disparar == true && recarga == true) {
-    balasX.push(posXDelDisparo)
+  posYDelDisparo=posY
+
+    if (disparar == true && recarga == true) {
+    balasX.push(posX+50)
       cuenta = 0
       recarga = false
   }
@@ -61,67 +62,353 @@ function disparo(posX, posY) {
       if (balasX[i] <= 0) {
       balasX.splice(i, 1)
     }
-    //circle(balasX[i], posY, 20)
-    ellipse(balasX[i], posYDelDisparo, 20, 20)
+    if (!okHit) {
+      push()
+        noStroke()
+        fill(234, 47, 47)
+        ellipse(balasX[i], posYDelDisparo, 20, 20)
+        posXDelDisparo=balasX[i]
+        pop()
+    }
   }
   cuenta++
     if (cuenta >= 30) {
     recarga = true
   }
   print("Cantidad de balas:", balasX.length)
- }
-function fondoEnMovimiento(){
- push()
- scale(2)
-  posXDelFondo-=5
-  if(posXDelFondo<=-width){
-  posXDelFondo=width
-  } 
-  image(fondo,posXDelFondo,0)
-pop()
 }
-/*function tiempo() {
- if (!okTiempo) {
- segundos = intervalo-int(millis()/1000)
- timempoEnPantalla = nf(minutos, 2)+":"+nf(segundos, 2)
- text(timempoEnPantalla, 200, 200)
- if ((segundos==0)&&(minutos==0)) {
- okTiempo=true
- minutos=3
- intervalo+=10
- } else if (segundos == 0) {
- if (minutos>0) {
- minutos--
- intervalo+=10
- } else {
- minutos=0
- intervalo+=10
- }
- }
- }
- }*/
-/*function enemigos(){
-switch 
-}
-}*/
-function botones() {
-  switch (pantallas) {
-  case 1:
-    generarBoton(780, 610, ancho, alto, colorBoton, "Play", 780, 610, colorTexto)
-      generarBoton(780, 710, ancho, alto, colorBoton, "Creditos", 780, 710, colorTexto)
-      break
-    case 3:
-    break
+
+function fondoEnMovimiento() {
+  push()
+    scale(2)
+    image(fondo, posXDelFondo, 0)
+    pop()
+    posXDelFondo-=5
+    if (posXDelFondo<=-fondo.width) {
+    posXDelFondo=window.innerWidth
   }
 }
 
-function generarBoton(posXBoton, posYBoton, ancho, alto, colorBoton, texto, posXText, posYText, colorTexto) {
+function interfaz() {
+  if (!okVidas) {
+    push()
+      fill(255, 127)
+      noStroke()
+      rect(0, 0, 1000, 200, 20)
+      pop()
+      tiempo()
+      corazones()
+      if (okTiempo) {
+      pantallas=4
+    }
+  } else {
+    pantallas=5
+      print("perdiste")
+  }
+}
+
+function tiempo() {
+  if (!okTiempo) {
+    segundos = intervalo-int(millis()/1000)
+      timempoEnPantalla = nf(minutos, 2)+":"+nf(segundos, 2)
+      push()
+      textSize(40)
+      text(timempoEnPantalla, 385, 65)
+      pop()
+      if ((segundos==0)&&(minutos==0)) {
+      okTiempo=true
+    } else if (segundos == 0) {
+      if (minutos>0) {
+        minutos--
+          intervalo+=10
+      } else {
+        minutos=0
+          intervalo+=10
+      }
+    }
+  }
+}
+function corazones() {
+  let x=25
+    let y=50
+    for (let i= 0; i<vidas.length; i++) {
+    if (okDanio) {
+      vidas.splice(i, 1)
+        cantVidas--
+    }
+    if (cantVidas>0) {
+      okDanio=false
+        push()
+        scale(0.09)
+        translate(400, 400)
+        image(vidas[i], x, y)
+        pop()
+        x+=1000
+    } else {
+      pantallas=5
+    }
+  }
+}
+function enemigos() {
+
+  for (let i = 0; i < 10; i++) {
+
+    if (enemigosX[i] > width) {
+      //obstaculosY[i] = random(-100, 0)
+
+
+      enemigosVel[i] = -enemigosVel[i]
+    }
+    if (enemigosX[i] < 0) {
+
+      enemigosX[i] =  width
+        enemigosVel[i] = enemigosVel[i]*-1
+    }
+    enemigosX[i]+=enemigosVel[i]
+      if (posXDelJugador > enemigosX[i]  && posXDelJugador < enemigosX[i] +  tam[i] &&
+      posYDelJugador > enemigosY[i] && posYDelJugador < enemigosY[i] +   tam[i]
+      ) {
+      fill(0)
+        if ( desaparecer[i]  == false) {
+        okDanio=true
+          contHit++
+      }
+      desaparecer[i] = true
+    }
+    if (dist(posXDelDisparo, posYDelDisparo, enemigosX[i], enemigosY[i])<20) {
+      fill(0)
+        if ( desaparecer[i]  == false) {
+        okHit=true
+          contHit++
+          print(contHit)
+      }
+      okHit=false
+        desaparecer[i] = true
+    } else {
+      fill(255)
+    }
+    if (desaparecer[i] == true) {
+    } else {
+      push()
+        image(naveEnemiga, enemigosX[i], enemigosY[i])
+        pop()
+    }
+    if (desaparecer[i] == true && contHit==10) {
+      iniciar()
+        contHit=0
+    }
+  }
+}
+function iniciar() {
+
+  for (let i = 0; i < 10; i++) {
+    enemigosY[i] = random(100, window.innerHeight)
+      enemigosVel[i] =  random(5, 10)
+      enemigosX[i] =  window.innerWidth
+      desaparecer[i] = false
+      tam[i] = 50
+  }
+}
+
+function inicio() {
   push()
-    fill(colorBoton)
-    stroke(10)
-    rect(posXBoton, posYBoton, ancho, alto)
-    textSize(tamTexto)
-    fill(colorTexto)
-    text(texto, posXText, posYText)
+    scale(2)
+    image(fondoInicial, 0, 0)
     pop()
+    push()
+    scale(0.3)
+    translate(2300, 1525)
+    image(botonInicio, 800, 440)
+    pop()
+    push()
+    scale(0.3)
+    translate(2250, 1825)
+    image(botonInstrucciones, 850, 540)
+    pop()
+    push()
+    scale(0.3)
+    translate(2220, 1825)
+    image(botonCreditos, 895, 940)
+    pop()
+    push()
+    fill(80, 234, 99)
+    text("guardian \nof\nthe earth", 925, 120)
+    pop()
+}
+function instrucciones() {
+  push()
+    scale(1)
+    translate(900, 500)
+    image(fondoInstrucciones, 0, 0)
+    pop()
+    push()
+    scale(0.3)
+    translate(2300, 1525)
+    image(botonMenu, 795, 740)
+    pop()
+    push()
+    textSize(50)
+    fill(80, 234, 99)
+    text("Movimiento: WASD", 925, 210)
+    pop()
+    push()
+    textSize(50)
+    fill(80, 234, 99)
+    text("Ataque: Space", 925, 350)
+    pop()
+    push()
+    textSize(50)
+    fill(80, 234, 99)
+    text("Objetivo: Sobrevivir a la invacion", 925, 510)
+    pop()
+}
+
+function creditos() {
+  push()
+    scale(0.5)
+    translate(1900, 1000)
+    image(fondoCreditos, 0, 0)
+    pop()
+    push()
+    fill(80, 234, 99)
+    textSize(50)
+    text("Programador: Julian Ferrari Goñi", 925, 210)
+    pop()
+    push()
+    textSize(40)
+    fill(80, 234, 99)
+    text("Diseño: Julian Ferrari Goñi", 925, 410)
+    pop()
+    push()
+    scale(0.3)
+    translate(2300, 1525)
+    image(botonMenu, 795, 740)
+    pop()
+}
+function pantallaGano() {
+  push()
+    scale(0.5)
+    translate(1900, 1000)
+    image(ganasteFondo, 0, 0)
+    pop()
+    push()
+    fill(80, 234, 99)
+    textSize(90)
+    text("La invacion ha sido destruida\nEl mundo descansa en paz", 925, 120)
+    pop()
+    push()
+    scale(0.3)
+    translate(2300, 1525)
+    image(botonMenu, 795, 740)
+    pop()
+    push()
+    textSize(20)
+    fill(80, 234, 99)
+    text("Pulsa R para reiniciar el juego", 925, 920)
+    pop()
+}
+
+function pantallaPerdio() {
+  push()
+    scale(0.5)
+    translate(1900, 1000)
+    image(perdisteFondo, 0, 0)
+    pop()
+    push()
+    fill(80, 234, 99)
+    textSize(90)
+    text("El mundo ha sido invadido\nLa humanidad esta perdida", 925, 120)
+    pop()
+    push()
+    scale(0.3)
+    translate(2300, 1525)
+    image(botonMenu, 795, 740)
+    pop()
+    push()
+    textSize(20)
+    fill(80, 234, 99)
+    text("Pulsa R para reiniciar el juego", 925, 920)
+    pop()
+}
+
+
+function interaccionDeBotones() {
+  if (pantallas==1) {
+    if ((mouseX>posXBotonIniciar-anchoBoton/2)&&(mouseX<+posXBotonIniciar+anchoBoton/2)&&(mouseY>posYBotonIniciar-altoBoton/2)&&(mouseY<posYBotonIniciar+altoBoton/2)) {
+      print("Boton Iniciar Activo")
+        okIniciar=true
+    } else {
+      okIniciar=false
+    }
+    if ((mouseX>posXBotonCreditos-anchoBoton/2)&&(mouseX<+posXBotonCreditos+anchoBoton/2)&&(mouseY>posYBotonCreditos-altoBoton/2)&&(mouseY<posYBotonCreditos+altoBoton/2)) {
+      print("Boton Credito Activo")
+        okCreditos=true
+    } else {
+      okCreditos=false
+    }
+    if ((mouseX>posXBotonInstrucciones-anchoBoton/2)&&(mouseX<+posXBotonInstrucciones+anchoBoton/2)&&(mouseY>posYBotonInstrucciones-altoBoton/2)&&(mouseY<posYBotonInstrucciones+altoBoton/2)) {
+      print("Boton Intrucciones Activo")
+        okInstrucciones=true
+    } else {
+      okInstrucciones=false
+    }
+  }
+  if ((pantallas==6)||(pantallas==4)||(pantallas==5)||(pantallas==2)) {
+    if ((mouseX>posXBotonMenu-anchoBoton/2)&&(mouseX<+posXBotonMenu+anchoBoton/2)&&(mouseY>posYBotonMenu-altoBoton/2)&&(mouseY<posYBotonMenu+altoBoton/2)) {
+      print("Boton Menu Activo")
+        okMenu=true
+    } else {
+      okMenu=false
+    }
+  }
+}
+
+function activacionDeBotones() {
+  if (okIniciar) {
+    pantallas=3
+      okIniciar=false
+  }
+  if (okCreditos) {
+    pantallas=6
+      okCreditos=false
+  }
+  if (okInstrucciones) {
+    pantallas=2
+      okInstrucciones=false
+  }
+  if (okMenu) {
+    recargarDatos()
+      pantallas=1
+      okMenu=false
+  }
+}
+
+function recargarDatos() {
+  for (let i=0; i<3; i++) {
+    vidas[i]=loadImage("./data/corazon.png")
+  }
+  velocidadFondo=3
+    posXDelFondo=0
+
+    cantVidas=3
+
+    posXDelJugador=100
+    posYDelJugador=500
+
+    okVidas=false
+    okTiempo=false
+    okGano=false
+    okDanio=false
+    disparar=false
+    recargar=true
+    okCreditos=false
+    okIniciar=false
+    okHit=false
+
+    iniciar()
+    contHit=0
+    minutos=2
+    intervalo=59+int(millis()/1000)
+    segundos=59
+    tiempoEnPantalla="3:00"
 }
--- tp4/Tp4.js
+++ tp4/Tp4.js
@@ -1,135 +1,192 @@
+/*
+Julian Ferrari Goñi
+93052/3
+https://youtu.be/CfinrwqMJZY
+ */
 let pantallas
 
-let fondo =[]
-let botonInicio
-let botonCreditos
+  let fondoPrincipal
+  let fondo =[]
+  let nave
+  let botonInicio
+  let botonCreditos
+  let botonVolver
+  let botonMenu
+  let botonInstrucciones
+  let ganasteFondo
+  let perdisteFondo
+  let fondoCreditos
+  let fondoInstrucciones
+  let naveEnemiga
 
-let okTiempo 
-let okVidas
-let okArriba
-let okAbajo
-let okIzquierda
-let okDerecha
-let okGano
-let disparar 
-let recarga = true
-let enemigosActivos
+  let titulo
 
-let posXDelJugador
-let posYDelJugador
-let posXDelDisparo
-let posYDelDisparo
+  let okTiempo
+  let okVidas
+  let okArriba
+  let okAbajo
+  let okIzquierda
+  let okDerecha
+  let okGano
+  let okDanio
+  let disparar
+  let recarga = true
+  let enemigosActivos
+  let okCreditos
+  let okIniciar
+  let okInstrucciones
+  let okMenu
+  let okHit
 
-let velocidadFondo
-let posXDelFondo
+  let contHit
+  let posXDelJugador
+  let posYDelJugador
+  let posXDelDisparo
+  let posYDelDisparo
+  let posXBotonIniciar
+  let posYBotonIniciar
+  let posXBotonCreditos
+  let posYBotonCreditos
+  let posXBotonInstrucciones
+  let posYBotonInstrucciones
+  let posXBotonMenu
+  let posYBotonMenu
 
-let posXDatos
-let posYDatos
+  let velocidadFondo
+  let posXDelFondo
 
-let w, h
-let tamanio,ancho, alto
+  let posXDatos
+  let posYDatos
 
-let vidas = []
-let enemigos= []
+  let w, h
+  let tamanio, ancho, alto, anchoBoton, altoBoton
 
-let tiempoEnPantalla
-let segundos 
-let minutos
-let intervalo
+  let vidas = []
+  let cantVidas
 
-let colorBoton,colorTexto
 
- 
-let cuenta 
-let balasX = []
+  let enemigosX= []
+  let enemigosY=[]
+  let enemigosVel=[]
+  let desaparecer=[]
+  let tam=[]
 
-function preload(){//carga de imagenes
-fondo= loadImage("./data/fondo.jpg")
+  let tiempoEnPantalla
+  let segundos
+  let minutos
+  let intervalo
+  let segundero
+
+  let colorBoton, colorTexto
+
+
+  let cuenta
+  let balasX = []
+
+  function preload() {//carga de imagenes
+  titulo=loadFont("./data/Crang.ttf")
+    fondoInicial=loadImage("./data/fondoInicio.png")
+    botonCreditos=loadImage("./data/botonCreditos.png")
+    botonInstrucciones=loadImage("./data/botonInstrucciones.png")
+    botonVolver=loadImage("./data/botonVolver.png")
+    botonMenu=loadImage("./data/botonMenu.png")
+    fondo= loadImage("./data/fondo.jpg")
+    nave=loadImage("./data/nave.png")
+    naveEnemiga=loadImage("./data/naveEnemiga.png")
+    ganasteFondo=loadImage("./data/ganaste.jpg")
+    perdisteFondo=loadImage("./data/perdiste.jpg")
+    fondoCreditos=loadImage("./data/fondoCreditos.jpg")
+    botonInicio=loadImage("./data/botonStar.png")
+    fondoInstrucciones=loadImage("./data/fondoInstrucciones.jpg")
+    for (let i=0; i<3; i++) {
+    vidas[i]=loadImage("./data/corazon.png")
+  }
 }
 
 
-function setup() {   
+function setup() {
+
   pantallas=1
-  
+
+    cantVidas=3
+    contHit=0
     w=window.innerWidth
     h=window.innerHeight
 
     tamanio=100
     ancho=100
     alto=50
+    anchoBoton=220
+    altoBoton=100
 
     posXDelJugador=100
     posYDelJugador=500
-    
+
+    posXBotonIniciar=936
+    posYBotonIniciar=588
+
+    posXBotonCreditos=927
+    posYBotonCreditos=869
+
+    posXBotonInstrucciones=929
+    posYBotonInstrucciones=704
+
+    posXBotonMenu=935
+    posYBotonMenu=678
+
     velocidadFondo=3
     posXDelFondo=0
- 
- 
+
+
 
     okVidas=false
     okTiempo=false
     okGano=false
+    okDanio=false
     disparar=false
     recargar=true
-    
-    
+    okCreditos=false
+    okIniciar=false
+    okInstrucciones=false
+    okMenu=false
+    okHit=false
+
+
 
     minutos=2
-    intervalo=60
-    segundos=0
+    intervalo=59
+    segundos=59
     tiempoEnPantalla="3:00"
-    
+
     textSize(100)
+    textFont(titulo)
     textAlign(CENTER)
     rectMode(CENTER)
-   imageMode(CENTER)
-    
-    for(let i=0; i<balasX.length;i++){
-      balasX[i]= windowWidth
-    }
-    for (let i=1;i<=3;i++){
-      enemigos[i]=i
-    }
-    
+    imageMode(CENTER)
+
+    for (let i=0; i<balasX.length; i++) {
+    balasX[i]= windowWidth
+  }
+  for (let i=1; i<=3; i++) {
+    enemigos[i]=i
+  }
+  iniciar()
     createCanvas(w, h)
-    fondo.resize(w,h)
 }
 
 function draw() {
-  image(fondo,0,0)
   programa()
 }
 
-//function preload() {
-//      posXDelJugador=100
-//    posYDelJugador=500
- 
-    
 
-//    colorBoton=255
-//    colorTexto=0
-
-//    okVidas=false
-//    okTiempo=false
-//    okGano=false
-//    disparar=false
-//    recargar=true
-    
-    
-
-//    minutos=2
-//    intervalo=60
-//    segundos=0
-//    tiempoEnPantalla="3:00"
-    
-   
-//}
 function mousePressed() {
-  pantallas++
-    print(mouseX, mouseY)
-    if (pantallas==5) {
-    pantallas=1;
-  }
+  print(mouseX, mouseY)
+
+    activacionDeBotones()
+}
+
+function mouseMoved() {
+  interaccionDeBotones()
 }
 function keyPressed() {
   if ((key=="A")||(key=="a")) {
@@ -144,14 +201,13 @@ function keyPressed() {
   if ((key=="S")||((key=="s"))) {
     okAbajo=true
   }
-  if(key==" "){
+  if (key==" ") {
     print("FUEGO!!!")
-  disparar=true
+      disparar=true
   }
-  if((key=="r")||(key=="R")){
-  preload()
-    pantallas=2
-
+  if ((key=="r")||(key=="R")) {
+    recargarDatos()
+      pantallas=3
   }
 }
 function keyReleased() {
@@ -167,8 +223,8 @@ function keyReleased() {
   if ((key=="S")||((key=="s"))) {
     okAbajo=false
   }
-  if(key==" "){
+  if (key==" ") {
     print("ALTO EL FUEGO!!")
-  disparar=false
+      disparar=false
   }
 }
