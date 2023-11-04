e7f432a2bcfffbea6ed3da4a82e7b1a48ae29aa3
julyjfg <80898439+julyjfg@users.noreply.github.com>
2023-11-04 17:39:05 -0300

    Add files via upload

--- tp5/Disparar.js
+++ tp5/Disparar.js
@@ -1,62 +1,71 @@
 class Disparar {
-  constructor(posX,posY) {
-    this.posXBalaConPersonaje=0
-      this.posYBalaConPersonaje=0
-      this.posYBala=0
-      this.balasX=[]
-      for (let i=0; i<this.balasX.length; i++) {
-      balasX[i]=windowWidth
-    }
-    this.recargar=true
+  constructor(posX, posY) {
+
+      this.balas=[]
+      this.recargar=true
       this.hit=false
       this.disparando=false
       this.tam=20
   }
-  dibujar(posX,posY) {
-    this.posXBalaConPersonaje=posX
+  dibujar(posX, posY, enemigos,enemigosEliminados) {
+    this.posXBalaConPersonaje = posX
     this.posYBalaConPersonaje=posY
-    this.posXBala=this.posXBalaConPersonaje
-    this.posYBala=this.posYBalaConPersonaje 
+
+
       this.disparo()
-      for (let i = 0; i < this.balasX.length; i++) {
-      push();
-      noStroke();
-      fill(255);
-      ellipse(this.balasX[i], this.posYBala, this.tam);
-      pop();
+
+      for (let i = this.balas.length - 1; i >= 0; i--) {
+      for (let j = enemigos.length - 1; j >= 0; j--) {
+        if (dist(this.balas[i].x,
+                  this.balas[i].y,
+                  enemigos[j].posX,
+                  enemigos[j].posY
+                  )< this.tam) {
+          this.balas.splice(i, 1)
+          enemigos.splice(j, 1)
+          // Incrementa el contador de enemigos eliminados en el Estado
+          enemigosEliminados++
+        }
+      }
+    }
+
+    // Dibuja las balas
+    for (let i = 0; i < this.balas.length; i++) {
+      push()
+      noStroke()
+      fill(255)
+      ellipse(this.balas[i].x, this.balas[i].y, this.tam)
+      pop()
     }
   }
 
   disparo() {
+    if (this.disparando && this.recargar) {
+      this.balas.push( {x:this.posXBalaConPersonaje, y:this.posYBalaConPersonaje})
+      this.recargar = false
+    }
 
-    
-      if (this.disparando && this.recargar) {
-      this.balasX.push(this.posXBala+50)
-        this.recargar = false
-    }
-    for (let i=0; i<this.balasX.length; i++) {
-      this.balasX[i]+=20
-        if (this.balasX[i]<=0) {
-        balasX.splice(i, 1)
-      }
-      if (this.balasX.length>0 && this.balasX[i]>=windowWidth) {
-        this.balasX.shift()//elimina la bala que sale de la pantalla
-          this.recargar=true//permite disparar la siguiente
+    for (let i = this.balas.length - 1; i >= 0; i--) {
+      this.balas[i].x += 20
+      if (this.balas[i].x <= 0 || this.balas[i].x >= windowWidth) {
+        this.balas.splice(i, 1)
+        this.recargar = true
       }
     }
-    print("Cantidad de balas:", this.balasX.length)
+    print("Cantidad de balas:", this.balas.length)
   }
+
   keyPressed() {
-    if (key==" ") {
-      this.disparando=true
-        print("BANG")
+    if (key === " ") {
+      this.disparando = true
+      print("BANG")
     }
   }
 
   keyReleased() {
-    if (key==" ") {
-      this.disparando=false
-        print("ALTO EL FUEGO")
+    if (key === " ") {
+      this.disparando = false
+      print("ALTO EL FUEGO")
     }
   }
 }
--- tp5/estado.js
+++ tp5/estado.js
@@ -2,14 +2,14 @@ class Estado {
 
   constructor() {
     this.pantalla=0
+      this.enemigosEliminados=0
       this.fondos= new Fondos()
       this.interfaz=new Interfaz()
       this.personaje=new Personaje()
-      this.disparo=new Disparar(this.personaje.posX,this.personaje.posX)
+      this.disparo=new Disparar(this.personaje.posX, this.personaje.posX)
       this.enemigo=[]
       for (let i=0; i<=5; i++) {
       this.enemigo[i]=new Enemigo()
-     
     }
   }
 
@@ -20,28 +20,36 @@ class Estado {
         break
       case 1:
       background(127)
-        this.interfaz.dibujar()
+        if (this.enemigo.length==0) {
+        // Todos los enemigos han sido eliminados, crea nuevos enemigos.
+        this.enemigo = [];
+        for (let i = 0; i < 5; i++) {
+          this.enemigo[i] = new Enemigo();
+        }
+      }
+      this.interfaz.dibujar()
         this.personaje.dibujar()
-        this.disparo.dibujar(this.personaje.posX,this.personaje.posY)
-        this.enemigo[2].dibujar()
-        if(dist(this.disparo.posXBala,this.disparo.posYBala,this.enemigo[2].posX,this.enemigo[2].posX)<this.disparo.tam){
-      print("hit")
-        }
-        if (this.interfaz.okTiempo) {
-          this.pantalla=2
-            this.interfaz.actualizar()
-        }
+        this.disparo.dibujar(this.personaje.posX, this.personaje.posY, this.enemigo,this.enemigosEliminados)
+        for (let i=0; i<=5; i++) {
+        this.enemigo[i].dibujar()
+      }
+      if (this.interfaz.okTiempo) {
+        this.pantalla=2
+          this.interfaz.actualizar()
+      }
       break
       case 2:
       this.fondos.fondoGanaste()
+        break
     }
   }
   mousePressed() {
-         if ((this.fondos.okBoton) && (this.pantalla=0)) {
+    if ((this.fondos.okBoton) && (this.pantalla=0)) {
       this.pantalla=1
     }
     if ((this.fondos.okBoton) && (this.pantalla=2)) {
       this.pantalla=1
+      this.enemigosEliminados=0
     }
   }
   mouseMoved() {
@@ -49,10 +57,10 @@ class Estado {
   }
   keyPressed() {
     this.personaje.keyPressed()
-    this.disparo.keyPressed()
+      this.disparo.keyPressed()
   }
   keyReleased() {
     this.personaje.keyReleased()
-    this.disparo.keyReleased()
+      this.disparo.keyReleased()
   }
 }
